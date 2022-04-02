8723e94d9730a2ec5c6863a89be589899f690182
Cristiano de Carvalho <cristiano.filho@dellead.com>
2022-04-02 20:59:33 -0300

    Creating method to save topics

--- forum/src/main/java/br/com/cristiano/forum/controller/TopicoController.java
+++ forum/src/main/java/br/com/cristiano/forum/controller/TopicoController.java
@@ -1,13 +1,20 @@
 package br.com.cristiano.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.cristiano.forum.dto.TopicoDto;
+import br.com.cristiano.forum.dto.TopicoInput;
 import br.com.cristiano.forum.modelo.Topico;
+import br.com.cristiano.forum.repository.CursoRepository;
 import br.com.cristiano.forum.repository.TopicoRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class TopicoController {
 
 	private final TopicoRepository topicoRepository;
+	private final CursoRepository cursoRepository;
 
 	@GetMapping
 	public List<TopicoDto> listar(String nomeCurso) {
@@ -27,6 +35,16 @@ public class TopicoController {
 			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
 			return TopicoDto.toListDto(topicos);
 		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoInput topicoInput, UriComponentsBuilder uriBuilder) {
+		Topico topico = topicoInput.toEntity(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+		
 	}
 
 }
--- forum/src/main/java/br/com/cristiano/forum/modelo/Topico.java
+++ forum/src/main/java/br/com/cristiano/forum/modelo/Topico.java
@@ -45,6 +45,12 @@ public class Topico {
 	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
 	
+	public Topico(String titulo, String mensagem, Curso curso) {
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
+	
 	public TopicoDto toDto() {
 		return new TopicoDto(this);
 	}
