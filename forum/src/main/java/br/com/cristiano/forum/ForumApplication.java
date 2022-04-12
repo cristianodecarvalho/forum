9085cd5d20250a6886195e5b1ee464231d3a7dda
Cristiano de Carvalho <cristiano.filho@dellead.com>
2022-04-12 18:47:53 -0300

    Adding environment variables to not externalize sensitive data from application.properties

--- forum/src/main/java/br/com/cristiano/forum/ForumApplication.java
+++ forum/src/main/java/br/com/cristiano/forum/ForumApplication.java
@@ -11,10 +11,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
+//Caso queira gerar um arquivo .war é necessário fazer as alterações no pom.xml e essa classe deve herdar de SpringBootServletInitializer
+//public class ForumApplication extends SpringBootServletInitializer {
 public class ForumApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
+	
+//	Metodo sobrescrito de SpringBootServletInitializer para gerar o .war
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(ForumApplication.class);
+//	}
 
 }
