52365b5f9a9af50570326989a00bad2bb3f03abe
Cristiano de Carvalho <cristiano.filho@dellead.com>
2022-04-12 20:26:24 -0300

    Exposing API Documentation

--- forum/src/main/java/br/com/cristiano/forum/config/security/SecurityConfiguration.java
+++ forum/src/main/java/br/com/cristiano/forum/config/security/SecurityConfiguration.java
@@ -51,6 +51,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
 		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+		.antMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
 		.antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
 		.anyRequest().authenticated()
 		.and().csrf().disable()
